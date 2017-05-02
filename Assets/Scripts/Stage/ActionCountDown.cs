9220f2dfb4a6d30720e9f09f7125e0253a4130f1
ngspmayx <ngspmayxdabadaba@gmail.com>
2017-05-02 18:42:59 +0900

    カウントダウンアクション途中

---
+++ Assets/Scripts/Stage/ActionCountDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionCountDown : MonoBehaviour {
+
+    private bool m_Action_flag = false;
+    [SerializeField]
+    private Vector3 m_destination = Vector3.zero;
+    [SerializeField]
+    private float m_move_time;
+
+    private float m_time_step = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_Action_flag)
+        {
+            m_autoMoveTime = Time.time;
+        }
+	}
+}
