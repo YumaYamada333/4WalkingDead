55b3bc3fcd0b632a76c5fb0fdf89ea5a9b13f3f8
TSITCL\s162158 <sutrikshoto@gmail.com>
2017-05-10 19:46:11 +0900

    エフェクト修正

--- Assets/Particle/ParticaleScript.cs
+++ Assets/Particle/ParticaleScript.cs
@@ -9,26 +9,28 @@ public class ParticaleScript : MonoBehaviour
     ParticleSystem particle;
 
     //パーティカルの種類
+    const int NONE = 0;
     const int MOVE = 1;
     const int ATTACK = 2;
     const int DAMAGE = 3;
     const int LANDING = 4;
 
-    const int GIMMICK = 0;
-    const int BREAK = 1;
+    //const int GIMMICK = 2;
+    //const int BREAK = 1;
 
     //パーティカル種類の判別用
     private int particleCnt;
-    private int BlockPartical;
-    private bool flag = false;
+    //private int BlockPartical;
+    //private bool flag = false;
 
     //プレイヤー取得用
     GameObject player;
     PlayerAction act;
     //アクションブロック取得用
     GameObject block;
-    BlockMove blockAct;
     ActionCountDown actDown;
+    //BlockAction blockAction;
+    //BlockMove blockAct;
 
     // Use this for initialization
     void Start ()
@@ -38,9 +40,10 @@ public class ParticaleScript : MonoBehaviour
 
         player = GameObject.Find("unitychan");
         act = player.GetComponent<PlayerAction>();
-        block = GameObject.Find("Block");
-        actDown = block.GetComponent<ActionCountDown>();
-
+        //block = GameObject.Find("Block");
+        //actDown = block.GetComponent<ActionCountDown>();
+        //blockAction = block.GetComponent<BlockAction>() as BlockAction;
+        //blockAct = blockAction.GetComponent<BlockMove>() as BlockMove;
 
         //パーティカルの停止
         particle.Stop();
@@ -51,16 +54,16 @@ public class ParticaleScript : MonoBehaviour
     {
         //プレイヤーのアクションを判別
         particleCnt = act.particleCnt;
-        BlockPartical = actDown.m_action_type;
-        flag = actDown.PartTim;
+        //BlockPartical = actDown.GetActionType();
+        //flag = blockAct.GetFlag();
 
         //アクションの種類によってパーティカルを発生
         switch (particleCnt)
         {
-            case 0:
+            case NONE:
                 particle.Stop();
                 break;
-            case 1:
+            case MOVE:
                 if (particle.name == "MoveParticle")
                 {
                     particle.Play();
@@ -70,7 +73,7 @@ public class ParticaleScript : MonoBehaviour
                     particle.Stop();
                 }
                 break;
-            case 2:
+            case ATTACK:
                 if (particle.name == "AttackParticle")
                 {
                     particle.Play();
@@ -80,7 +83,7 @@ public class ParticaleScript : MonoBehaviour
                     particle.Stop();
                 }
                 break;
-            case 3:
+            case DAMAGE:
                 if (particle.name == "DamageParticle")
                 {
                     particle.Play();
@@ -90,7 +93,7 @@ public class ParticaleScript : MonoBehaviour
                     particle.Stop();
                 }
                 break;
-            case 4:
+            case LANDING:
                 if (particle.name == "LandingParticle")
                 {
                     particle.Play();
@@ -102,22 +105,22 @@ public class ParticaleScript : MonoBehaviour
                 break;
         }
 
-        //アクションの種類によってパーティカルを発生
-        switch (BlockPartical)
-        {
-            case 0:
-                if ((particle.name == "GimmickParticle" || particle.name == "GimmickParticle (1)") && flag == true)
-                {
-                    particle.Play();
-                }
-                break;
-            //case 1:
-            //    if (particle.name == "BreakParticle" && flag == true)
-            //    {
-            //        particle.Play();
-            //    }
-            //    break;
-        }
+        ////アクションの種類によってパーティカルを発生
+        //switch (BlockPartical)
+        //{
+        //    case GIMMICK:
+        //        if ((particle.name == "GimmickParticle" || particle.name == "GimmickParticle (1)") && flag == true)
+        //        {
+        //            particle.Play();
+        //        }
+        //        break;
+        //    //case BREAK:
+        //    //    if (particle.name == "BreakParticle" && flag == true)
+        //    //    {
+        //    //        particle.Play();
+        //    //    }
+        //    //    break;
+        //}
 
     }
 
