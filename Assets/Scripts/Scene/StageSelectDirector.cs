b4fb8ccc9ea875c0a7345206df77b86e647bc640
Ryuna Tsuji <ronnelronnelronnel216@gmail.com>
2017-05-22 16:25:02 +0900

    ラープの補助関数 ループ変数

--- Assets/Scripts/Scene/StageSelectDirector.cs
+++ Assets/Scripts/Scene/StageSelectDirector.cs
@@ -21,24 +21,52 @@ public class StageSelectDirector : MonoBehaviour {
         public SceneName nextScene;             // 次のシーン
     }
 
+    // パンフレットの配置空間
+    struct PamphletSpace
+    {
+        public Vector3 pos;
+        public int pamphlietIndex;
+        public void Set(Vector3 pos, int index)
+        {
+            this.pos = pos;
+            pamphlietIndex = index;
+        }
+    }
+
     [SerializeField]
     private Vector3 m_zeroPos = new Vector3(-2.0f, -4.5f, 0.0f);// 一番前のパンフレット座標
     [SerializeField]
     private Vector3 m_pos = new Vector3(1.5f, 1.5f, 0.1f);      // 重なりのずれ
     [SerializeField]
     private Pamphlet[] m_pamphletData = new Pamphlet[5];        // パンフレットのデータ
-    private int m_selectPamphlet = 0;                           // 選択中のパンフレット
+    private MathClass.Looper m_selectPamphlet;                  // 選択中のパンフレット
 
     private GameObject[] m_pamphlet = new GameObject[5];        // パンフレット
+    PamphletSpace[] m_space;                                    // パンフレットの配置空間
 
+    private Vector2 dragVecOld;
+
+    [SerializeField]
+    private float stepSpd = 0.01f;
+    private float changeStep = 0.0f;                            // 切り替えのステップ
 	// Use this for initialization
 	void Start ()
     {
-        // 初期パンフレットの配置
+        // ループ変数初期化
+        m_selectPamphlet = new MathClass.Looper(m_pamphlet.Length - 1, 0, 0, 0);
+
+        // パンフレット配置位置の配列の生成
+        m_space = new PamphletSpace[m_pamphlet.Length];
+
+        // マウスのドラック量を初期化
+        dragVecOld = GetComponent<MouseSystem>().GetDragVec();
+
+        // 初期パンフレットの配置 配置空間の設定
         for (int i = 0; i < m_pamphlet.Length; i++)
         {
             m_pamphlet[i] = Instantiate(m_pamphletData[i].pamphletPrefab);
-            m_pamphlet[i].transform.position = m_zeroPos + (m_pos * i);
+            m_space[i].Set(m_zeroPos + (m_pos * i), i);
+            m_pamphlet[i].transform.position = m_space[m_space[i].pamphlietIndex].pos;
         }
 
         // パンフが下から、リボンが上から規定位置まで移動
@@ -48,34 +76,55 @@ public class StageSelectDirector : MonoBehaviour {
     void Update ()
     {
         // マウスのドラック量を取得
-        Vector2 dragVec = GetComponent<MouseSystem>().GetDragVec();
+        Vector2 dragVec = GetComponent<MouseSystem>().GetDragVec() - dragVecOld;
 
-        if (dragVec.y < -30)
+        // 切り替えが行われていない
+        if (changeStep <= 1.0f)
         {
-            // 一番前のパンフが下に移動
+            if (Input.GetKeyDown(KeyCode.DownArrow)/*dragVec.y < -30*/)
+            {
+                // 一番前のパンフが下に移動
 
-            // 他のパンフが手前に移動
+                // 他のパンフが手前に移動
 
-            // 一番後ろにパンフが追加
+                // 一番後ろにパンフが追加
 
-            m_selectPamphlet++;
+                // 配置空間のパンフインデックスの更新
+                for (int i = 0; i < m_space.Length; i++)
+                {
+                    m_selectPamphlet.Plus(1);
+                    m_space[i].pamphlietIndex = m_selectPamphlet.Get();
+                }
+                changeStep = 0.0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow)/*dragVec.y > 30*/)
+            {
+                // 一番後ろのパンフが下に移動
+
+                // 他のパンフが奥に移動
+
+                // 一番前に下からパンフ挿入
+
+                // 配置空間のパンフインデックスの更新
+                for (int i = 0; i < m_space.Length; i++)
+                {
+                    m_selectPamphlet.Plus(-1);
+                    m_space[i].pamphlietIndex = m_selectPamphlet.Get();
+                }
+                changeStep = 0.0f;
+            }
         }
-        else if (dragVec.y > 30)
-        {
-            // 一番後ろのパンフが下に移動
+        else changeStep = changeStep + stepSpd > 1.0f ? 1.0f : changeStep + stepSpd;
 
-            // 他のパンフが奥に移動
+        Debug.Log(m_selectPamphlet.Get());
+        //// 範囲外にならないように
+        //if (m_selectPamphlet >= m_pamphletData.Length)
+        //    m_selectPamphlet = 0;
+        //if (m_selectPamphlet < 0)
+        //    m_selectPamphlet = m_pamphletData.Length - 1;
 
-            // 一番前に下からパンフ挿入
-
-            m_selectPamphlet--;
-        }
-
-        // 範囲外にならないように
-        if (m_selectPamphlet >= m_pamphletData.Length)
-            m_selectPamphlet = 0;
-        if (m_selectPamphlet < 0)
-            m_selectPamphlet = m_pamphletData.Length - 1;
+        // 前フレーム更新
+        dragVecOld = GetComponent<MouseSystem>().GetDragVec();
     }
 
     // ボタンを押したときの処理
@@ -93,6 +142,6 @@ public class StageSelectDirector : MonoBehaviour {
     // シーンの遷移
     private void StageScene()
     {
-        SceneManager.LoadScene(m_pamphletData[m_selectPamphlet].nextScene.ToString());
+        SceneManager.LoadScene(m_pamphletData[m_selectPamphlet.Get()].nextScene.ToString());
     }
 }
